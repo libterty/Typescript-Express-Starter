bddf261b68d894789f5e319b418fa24ec0dbe1ba
Albert Li <Albert.Li@isapsolution.com>
2020-04-24 11:33:18 +0800

    more output def

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 const app: express.Application = express();
 const port: number | string = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
+app.get('/', (req: Request, res: Response, next: NextFunction): void => {
     res.send('hello World!');
 });
 
