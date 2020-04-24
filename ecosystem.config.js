c77c7418c4f83dd819fd018efe96880e35db1a3c
Albert Li <Albert.Li@isapsolution.com>
2020-04-24 14:35:12 +0800

    convert start script without compile

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,19 @@
 module.exports = {
     apps: [
         {
-            name: 'ISAP API',
-            script: 'dist/index.js',
-            instance: 4,
+            name: 'ISAPAPI',
+            kill_timeout: 10000,
+            script: './src/index.ts',
+            instances: 4,
+            exec_mode: 'cluster',
             autorestart: true,
+            wait_ready: true,
             watch: false,
+            watch_options: {
+                usePolling: true,
+            },
+            error_file: './err.log',
+            out_file: './out.log',
             node_args: [],
             env: {
                 NODE_ENV: 'development',
